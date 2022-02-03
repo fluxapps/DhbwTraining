cb32ba09349ac7f131d08d52cf46da085df16e9f
fluxms <martin@fluxlabs.ch>
2022-02-03 02:52:11 +0100

    ilias7

--- plugin.php
+++ plugin.php
@@ -3,11 +3,11 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "xdht";
-$version = "0.0.11";
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "6.999";
-$responsible = "Martin Studer";
-$responsible_mail = "support@studer-raimann.ch";
+$version = "1.0.0";
+$ilias_min_version = "6.0";
+$ilias_max_version = "7.999";
+$responsible = "fluxlabs ag";
+$responsible_mail = "support@fluxlabs.ch";
 $learning_progress = true;
 $supports_export = true;
 ?>
